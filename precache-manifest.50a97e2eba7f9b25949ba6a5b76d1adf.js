74380efcb7f4bd3d4ab7c8f216debbeec58c26cf
IndradevKr <indradevkr8@gmail.com>
2022-04-06 11:24:58 +0545

    Updates

---
+++ precache-manifest.50a97e2eba7f9b25949ba6a5b76d1adf.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "371481aaa20304f7bf52",
+    "url": "/about/static/css/main.573d89e1.chunk.css"
+  },
+  {
+    "revision": "371481aaa20304f7bf52",
+    "url": "/about/static/js/main.371481aa.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/about/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "8e41bf3af43b502e0675",
+    "url": "/about/static/css/2.8a3d8912.chunk.css"
+  },
+  {
+    "revision": "8e41bf3af43b502e0675",
+    "url": "/about/static/js/2.8e41bf3a.chunk.js"
+  },
+  {
+    "revision": "f78a874cd7153843d251",
+    "url": "/about/static/js/runtime~main.f78a874c.js"
+  },
+  {
+    "revision": "a7c3352ff504a0294196ed74dca08af6",
+    "url": "/about/static/media/Indradev.a7c3352f.png"
+  },
+  {
+    "revision": "cbd75e37749b46d9f1ae1190ec3f3b5e",
+    "url": "/about/static/media/about_profile.cbd75e37.png"
+  },
+  {
+    "revision": "7bbc1100221630592895e3196338c772",
+    "url": "/about/static/media/imtechseeker.7bbc1100.png"
+  },
+  {
+    "revision": "1f40e04fc3254b15da8e2d841c95019b",
+    "url": "/about/static/media/hamrobajar.1f40e04f.png"
+  },
+  {
+    "revision": "ba9b03c3a5cb9937cdadfcb37328bccd",
+    "url": "/about/static/media/keeptodo.ba9b03c3.png"
+  },
+  {
+    "revision": "7a39e454a71575b45536a0d118f5bd67",
+    "url": "/about/static/media/iWeather.7a39e454.png"
+  },
+  {
+    "revision": "bd892ab72d92a005fba7a89561ce8196",
+    "url": "/about/static/media/portfolio_2021.bd892ab7.png"
+  },
+  {
+    "revision": "f950d750c703ad10508da5f1e3e4ae50",
+    "url": "/about/static/media/portfolio_2020.f950d750.png"
+  },
+  {
+    "revision": "8df33fbd749533f58aa0592777d3211f",
+    "url": "/about/static/media/youtube.8df33fbd.png"
+  },
+  {
+    "revision": "94659086ad249b91b5579deb463508ba",
+    "url": "/about/static/media/linkedIn.94659086.png"
+  },
+  {
+    "revision": "f53d8e1720b015b3c8cbe51dda3e68dc",
+    "url": "/about/static/media/github.f53d8e17.png"
+  },
+  {
+    "revision": "cdb4771befb4f1451676f698b54c73c3",
+    "url": "/about/static/media/whatsapp.cdb4771b.png"
+  },
+  {
+    "revision": "d83309ce276fe236ac8d90c420f26c36",
+    "url": "/about/static/media/facebook.d83309ce.png"
+  },
+  {
+    "revision": "894ebbce28357925b0046ef0ec9b921f",
+    "url": "/about/static/media/Indradev_Kumar_Mahato_Resume.894ebbce.pdf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/about/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/about/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/about/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/about/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a15fb6599ec8594bc52df0b3b2d6b79a",
+    "url": "/about/index.html"
+  }
+];
